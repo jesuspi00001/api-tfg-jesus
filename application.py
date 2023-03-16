3ae80a4fad5f8bc41fc557db79f468c37125f3e6
jesuspi00001 <jesuspi00001@gmail.com>
2023-03-16 13:16:11 +0100

    Inserto código HTML en la respuesta

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/',methods=['GET','POST'])
 def index():
-    return "Hola mundo v.2.0."
+    return "<h1>Hola mundo </h1> v.2.0."
 
 if __name__=="__main__":
     application.run()
