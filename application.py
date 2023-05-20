5c6493f0f13918698ac417eb770adbd7830bd101
jesuspi00001 <jesuspi00001@gmail.com>
2023-05-21 01:43:30 +0200

    Primer boceto vista inicial de BasketApp.

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route('/',methods=['GET','POST'])
 def index():
-    return "<h1>Hola mundo </h1> v.2.0."
+    title = 'BasketApp'
+    image_url = 'static/imagen2.jpg'
+    description = '¿Qué vamos a encontrar aquí? Con esta aplicación podrás obtener datos de diferente índole sobre la liga de balocensto profesional americana, que aglutina los mejores jugadores del mundo.'
+    return render_template('index.html', title=title, image_url=image_url, description=description)
 
 if __name__=="__main__":
     application.run()
