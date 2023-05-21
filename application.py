2143ab23d33b48072aab2b91152f903c04296977
jesuspi00001 <jesuspi00001@gmail.com>
2023-05-22 01:18:33 +0200

    Nueva versión de estilos usando bootstrap.

--- application.py
+++ application.py
@@ -4,22 +4,22 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    title = 'BasketApp'
-    image_url = 'static/imagen2.jpg'
+    title = 'Inicio'
+    image_url = 'static/imagen.jpg'
     description = '¿Qué vamos a encontrar aquí? Con esta aplicación podrás obtener datos de diferente índole sobre la liga de balocensto profesional americana, que aglutina los mejores jugadores del mundo.'
     return render_template('index.html', title=title, image_url=image_url, description=description)
 
 @application.route('/busqueda')
 def busqueda():
-    title = 'Búsqueda'
+    title = 'Búsqueda de estadísticas de la NBA'
     image_url = 'static/imagen.jpg'
     description = 'Vista dónde pondremos las busquedas a la base de datos SportsDataBase API.'
     return render_template('busqueda.html', title=title, image_url=image_url, description=description)
 
 @application.route('/quienes_somos')
 def quienes_somos():
-    title = '¿Quiénes somos?'
-    image_url = 'static/imagen3.jpg'
+    title = '¿Quién soy?'
+    image_url = 'static/imagen.jpg'
     description = 'Posible vista con una presentación mía. Soy Jesús Pereira Ibáñez, alumnos de Ingeniería Informática blablabla'
     return render_template('quienes_somos.html', title=title, image_url=image_url, description=description)
 
