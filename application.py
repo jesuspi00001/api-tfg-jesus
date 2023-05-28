aac1c4d7fa3bab1450e79015a206356d634c98f0
jesuspi00001 <jesuspi00001@gmail.com>
2023-05-28 07:19:09 +0200

    Metemos poco a poco las funcionalidades para ver cual falla en aws.

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 application = Flask(__name__)
 
@@ -8,8 +9,22 @@ def index():
 
 @application.route('/jugador_puntos', methods=['GET', 'POST'])
 def jugador_puntos():
+    if request.method == 'POST':
+        jugador = request.form['jugador']
+        temporada = request.form['temporada']
+        resultado = buscar_jugador(jugador, temporada)
+        return render_template('jugador_puntos.html', resultado=resultado[0], flag=resultado[1])
     
     return render_template('jugador_puntos.html')
+
+
+## Funcion busqueda de puntos x jugador en x temporada
+def buscar_jugador(jugador, temporada):
+
+    if jugador == 'Andrea':
+        return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
+    else:
+        return ["El jugador " + jugador + " tiene una media de tropecientos puntos en la temporada del año " + temporada + ".", True]
 
 
 if __name__=="__main__":
