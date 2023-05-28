b9f827633ad2b975a2d0171b4ede2d882eb568cd
jesuspi00001 <jesuspi00001@gmail.com>
2023-05-28 08:23:24 +0200

    Y si instalo solo get de requests?

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-import requests
 from flask import Flask, render_template, request
+from requests import get
 
 application = Flask(__name__)
 
@@ -9,6 +9,7 @@ def index():
 
 @application.route('/jugador_puntos', methods=['GET', 'POST'])
 def jugador_puntos():
+    
     if request.method == 'POST':
         jugador = request.form['jugador']
         temporada = request.form['temporada']
@@ -20,11 +21,34 @@ def jugador_puntos():
 
 ## Funcion busqueda de puntos x jugador en x temporada
 def buscar_jugador(jugador, temporada):
+    url_player = f"https://www.balldontlie.io/api/v1/players?search={jugador}"
 
-    if jugador == 'Andrea':
-        return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
+    player = get(url_player)
+    points=-1
+
+    if player.status_code == 200 and jugador!="":
+        data_player = player.json()
+        if data_player['data']:
+            player_id = data_player['data'][0]['id']
+
+            url_puntos = f"https://www.balldontlie.io/api/v1/season_averages?player_ids[]={player_id}&season={temporada}"
+ 
+            puntos = get(url_puntos)
+            
+            if puntos.status_code == 200:
+                data_puntos = puntos.json()
+                if data_puntos['data']:
+                    points = data_puntos['data'][0]['pts']
+
+        else:
+            return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
     else:
-        return ["El jugador " + jugador + " tiene una media de tropecientos puntos en la temporada del año " + temporada + ".", True]
+        return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
+
+    if points == -1:
+        return [jugador + " no jugó durante la temporada introducida en la NBA. Introduce una temporada en la que " + jugador + " formara parte de la liga.", False]
+    else:
+        return ["El jugador " + jugador + " tiene una media de: " + str(points) + " puntos en la temporada del año " + temporada + ".", True]
 
 
 if __name__=="__main__":
