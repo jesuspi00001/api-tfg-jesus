0272b3f80b18829f94aa153fa1462dd912d21ffa
jesuspi00001 <jesuspi00001@gmail.com>
2023-05-28 07:06:15 +0200

    Intento de arreglo.

--- application.py
+++ application.py
@@ -9,47 +9,46 @@ def index():
 
 @application.route('/jugador_puntos', methods=['GET', 'POST'])
 def jugador_puntos():
-    print(request.method)
     
-    if request.method == 'POST':
-        jugador = request.form['jugador']
-        temporada = request.form['temporada']
-        resultado = buscar_jugador(jugador, temporada)
-        return render_template('jugador_puntos.html', resultado=resultado[0], flag=resultado[1])
+    # if request.method == 'POST':
+    #     jugador = request.form['jugador']
+    #     temporada = request.form['temporada']
+    #     resultado = buscar_jugador(jugador, temporada)
+    #     return render_template('jugador_puntos.html', resultado=resultado[0], flag=resultado[1])
     
     return render_template('jugador_puntos.html')
 
 
 ## Funcion busqueda de puntos x jugador en x temporada
-def buscar_jugador(jugador, temporada):
-    url_player = f"https://www.balldontlie.io/api/v1/players?search={jugador}"
+# def buscar_jugador(jugador, temporada):
+#     url_player = f"https://www.balldontlie.io/api/v1/players?search={jugador}"
 
-    player = requests.get(url_player)
-    points=-1
+#     player = requests.get(url_player)
+#     points=-1
 
-    if player.status_code == 200 and jugador!="":
-        data_player = player.json()
-        if data_player['data']:
-            player_id = data_player['data'][0]['id']
+#     if player.status_code == 200 and jugador!="":
+#         data_player = player.json()
+#         if data_player['data']:
+#             player_id = data_player['data'][0]['id']
 
-            url_puntos = f"https://www.balldontlie.io/api/v1/season_averages?player_ids[]={player_id}&season={temporada}"
+#             url_puntos = f"https://www.balldontlie.io/api/v1/season_averages?player_ids[]={player_id}&season={temporada}"
  
-            puntos = requests.get(url_puntos)
+#             puntos = requests.get(url_puntos)
             
-            if puntos.status_code == 200:
-                data_puntos = puntos.json()
-                if data_puntos['data']:
-                    points = data_puntos['data'][0]['pts']
+#             if puntos.status_code == 200:
+#                 data_puntos = puntos.json()
+#                 if data_puntos['data']:
+#                     points = data_puntos['data'][0]['pts']
 
-        else:
-            return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
-    else:
-        return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
+#         else:
+#             return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
+#     else:
+#         return ["Alguno de los parámetros introducidos no es correcto. Escribe un nombre de jugador válido y una temporada entre 1946-actual.", False]
 
-    if points == -1:
-        return [jugador + " no jugó durante la temporada introducida en la NBA. Introduce una temporada en la que " + jugador + " formara parte de la liga.", False]
-    else:
-        return ["El jugador " + jugador + " tiene una media de: " + str(points) + " puntos en la temporada del año " + temporada + ".", True]
+#     if points == -1:
+#         return [jugador + " no jugó durante la temporada introducida en la NBA. Introduce una temporada en la que " + jugador + " formara parte de la liga.", False]
+#     else:
+#         return ["El jugador " + jugador + " tiene una media de: " + str(points) + " puntos en la temporada del año " + temporada + ".", True]
 
 
 if __name__=="__main__":
